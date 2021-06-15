95eacf3c5b7099dcd61cf933e7645d4ca2240c5e
Codecov Devops <devops@codecov.local>
2021-06-14 17:54:17 -0700

    added staging configs

--- client/cypress/integration/homepage.spec.js
+++ client/cypress/integration/homepage.spec.js
@@ -1,14 +1,26 @@
+const CELTICS = Cypress.env("celtics")
+
 describe("renders the home page", () => {
+  beforeEach(() => {
+    cy.visit("/")
+  })
+
   it("renders correctly", () => {
     // Refers to the baseurl
-    cy.visit("/")
     cy.get("#container").should("exist")
   })
 
-  it("allows the date picker to be used", () => {
-    cy.visit("/");
+  it("allows the date picker to be used", () => {;
     cy.get('#date').click();
     cy.get('#date').type('2021-02-17');
-    cy.findByText("Boston Celtics").should("exist");
+    cy.findByText(CELTICS).should("exist");
+  });
+
+  it("routes to a team's page", () => {
+    cy.get('#date').type('2021-02-17');
+    cy.findByText(CELTICS).should("exist");
+    cy.findAllByText(CELTICS).click()
+    cy.url().should("include", "teams/2");
+    cy.findByText("Conference: East").should("exist")
   })
 })
